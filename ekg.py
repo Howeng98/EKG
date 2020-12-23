7127de49c111620ea3df2dc2b0cb5771558f1e29
howeng98 <leonhwlee9@gmail.com>
2020-12-24 01:19:02 +0800

    question1 done

--- ekg.py
+++ ekg.py
@@ -1,4 +1,3 @@
-
 # from google.colab import drive
 # drive.mount('/content/drive/')
 # !nvidia-smi
@@ -12,8 +11,6 @@ from sklearn.model_selection import train_test_split
 from torchvision.datasets import ImageFolder
 from tqdm import tqdm
 
-
-data_dir = 'dataset'
 input, target = [], []
 
 def showImage(windowname,image):
@@ -26,97 +23,46 @@ def preprocessing(data_dir):
 	classes = dataset.classes
 	print(len(dataset.imgs)) # 600 images
 
-	for i in tqdm(range(len(dataset))):
-		input.append(dataset.imgs[i][0])
-		target.append(dataset.imgs[i][1])
-
-	x_train, x_test, y_train, y_test = train_test_split(input, target, test_size=0.2, random_state=42)
-	print(len(x_train))	
-	print(len(y_train))
-	print(len(x_test))
-	print(len(y_test))
-
 	# example
-	print(dataset.imgs[1][0])
-	print(dataset.imgs[1][1])
-	img = cv2.imread(dataset.imgs[1][0],cv2.IMREAD_GRAYSCALE)		
-	cropping(img, dataset.imgs[1][0], dataset.imgs[1][1])
+	# print(dataset.imgs[1][0])
+	# print(dataset.imgs[1][1])
 
-def cropping(image, image_path, image_label):
-	img_save_path = image_path[:-4] + '/'
+	lead_matrix = ['none'] * len(dataset)
+	for idx in range(len(dataset)):
+		img = cv2.imread(dataset.imgs[idx][0],cv2.IMREAD_GRAYSCALE)
+		lead_matrix = cropping(lead_matrix, idx, img)
+
+def cropping(lead_matrix, idx, image):
 	
 	# top_1
-	crop = image[390:512, 115:423]
-	crop = cv2.resize(crop, (128, 128))
-	# showImage('crop', crop)
-	
-	# cv2.imwrite(os.path.join(img_save_path , image_path[:] + '_top1' + '.png'), crop)
-	
+	top1 = image[390:512, 115:423]
 	# middle_1
-	crop = image[512:634, 115:423]
-	crop = cv2.resize(crop, (128, 128))
-	# showImage('crop', crop)
-	# cv2.imwrite(image_path[:] + '_middle1' + '.png', crop)
-
+	mid1 = image[512:634, 115:423]
 	# bottom_1
-	crop = image[634:756, 115:423]
-	crop = cv2.resize(crop, (128, 128))
-	# showImage('crop', crop)
-	# cv2.imwrite(image_path[:] + '_bottom1' + '.png', crop)
-
+	bot1 = image[634:756, 115:423]
 	# top_2
-	crop = image[390:512, 427:727]
-	crop = cv2.resize(crop, (128, 128))
-	# showImage('crop', crop)
-	# cv2.imwrite(image_path[:] + '_top2' + '.png', crop)
-
+	top2 = image[390:512, 427:727]
 	# middle_2
-	crop = image[512:634, 427:727]
-	crop = cv2.resize(crop, (128, 128))
-	# showImage('crop', crop)
-	# cv2.imwrite(image_path[:] + 'middle2' + '.png', crop)
-	
+	mid2 = image[512:634, 427:727]
 	# bottom_2
-	crop = image[634:756, 427:727]
-	crop = cv2.resize(crop, (128, 128))
-	# showImage('crop', crop)
-	# cv2.imwrite(image_path[:] + '_bottom2' + '.png', crop)
-	
+	bot2 = image[634:756, 427:727]
 	# top_3
-	crop = image[390:512, 731:1031]
-	crop = cv2.resize(crop, (128, 128))
-	# showImage('crop', crop)
-	# cv2.imwrite(image_path[:] + '_top3' + '.png', crop)
-	
+	top3 = image[390:512, 731:1031]
 	# middle_3
-	crop = image[512:634, 731:1031]
-	crop = cv2.resize(crop, (128, 128))
-	# showImage('crop', crop)
-	# cv2.imwrite(image_path[:] + '_middle3' + '.png', crop)
-	
+	mid3 = image[512:634, 731:1031]
 	# bottom_3
-	crop = image[634:756, 731:1031]
-	crop = cv2.resize(crop, (128, 128))
-	# showImage('crop', crop)
-	# cv2.imwrite(image_path[:] + '_bottom3' + '.png', crop)
-	
+	bot3 = image[634:756, 731:1031]
 	# top_4
-	crop = image[390:512, 1035:1335]
-	crop = cv2.resize(crop, (128, 128))
-	# showImage('crop', crop)
-	# cv2.imwrite(image_path[:] + '_top4' + '.png', crop)
-	
+	top4 = image[390:512, 1035:1335]
 	# middle_4
-	crop = image[512:634, 1035:1335]
-	crop = cv2.resize(crop, (128, 128))
-	# showImage('crop', crop)
-	# cv2.imwrite(image_path[:] + '_middle4' + '.png', crop)
-
+	mid4 = image[512:634, 1035:1335]
 	# bottom_4
-	crop = image[634:756, 1035:1335]
-	crop = cv2.resize(crop, (128, 128))
-	# showImage('crop', crop)
-	# cv2.imwrite(image_path[:] + '_bottom4' + '.png', crop)
+	bot4 = image[634:756, 1035:1335]
+	
+	lead_matrix[idx] = [top1,mid1,bot1,top2,mid2,bot2,top3,mid3,bot3,top4,mid4,bot4]
+	return lead_matrix
 
-preprocessing(data_dir)
 
+if __name__ == "__main__":
+	data_dir = 'dataset'
+	preprocessing(data_dir)
